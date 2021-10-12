c43fefa3ef2dea2878ea76e876e520a3e777442d
StoleYourFridge <stoleyourfridge@gmail.com>
2021-10-12 23:37:25 +0300

    truth table, tests and translation(possibly) remaining

--- АОИС_2.cpp
+++ АОИС_2.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <string>
 #include <vector>
 #include <cmath>
 #include <stack>
@@ -54,7 +55,7 @@ void sknf(vector<vector<bool>> term)
 	cout << ";" << endl;
 }
 
-int translate(vector<bool> term)
+int calculator(vector<bool> term)
 {
 	int result = 0, index = term.size()  - 1;
 	for (int i = 0; i < term.size(); i++)
@@ -70,7 +71,7 @@ void numeral(vector<vector<bool>> term)
 	cout << "(";
 	for (int i = 0; i < term.size(); i++)
 	{
-		cout << translate(term[i]);
+		cout << calculator(term[i]);
 		if (i != term.size() - 1) cout << ",";
 	}
 	cout << ");" << endl;
@@ -108,17 +109,111 @@ bool encode(string reverse, vector<bool>triple)
 	return result.top();
 }
 
+void deny(string& name)
+{
+	for (int i = 0; i < name.size(); i++)
+	{
+		if (name[i] == ' ') {
+			name.erase(name.begin() + i);
+			i--;
+		}
+	}
+
+	for (int i = 0; i < name.size(); i++)
+	{
+		if (name[i] == '!' && name[i + 1] == '(') {
+			name.erase(name.begin() + i);
+			stack<char>stack;
+			stack.push('(');
+			int index = i + 1;
+			while (stack.size() != 0)
+			{
+				if (name[index] == '(') stack.push('(');
+				else if (name[index] == ')') stack.pop();
+				index++;
+			}
+			name.insert(name.begin() + index, '!');
+		}
+		else if (name[i] == '!' && (name[i + 1] == 'a' || name[i + 1] == 'b' || name[i + 1] == 'c')) {
+			name.erase(name.begin() + i);
+			name.insert(name.begin() + i + 1, '!');
+		}
+	}
+}
+
+string translate()
+{
+	string name;
+	cout << "Enter your function : ";
+	getline(cin , name);
+	deny(name);
+	stack<char> stack;
+	string result;
+	for (int i = 0; i < name.size(); i++)
+	{
+		if (name[i] == 'a' || name[i] == 'b' || name[i] == 'c') {
+			result.push_back(name[i]);
+		}
+		else if (name[i] == '!') result.push_back('!');
+		else if (name[i] == ')') {
+			while (stack.top() != '(')
+			{
+				result.push_back(stack.top());
+				stack.pop();
+			}
+			stack.pop();
+			if (stack.size() != 0 && stack.top() == '*') {
+				if (name[i + 1] != '!') {
+					result.push_back('*');
+					stack.pop();
+				}
+				else {
+					result.push_back(name[i + 1]);
+					result.push_back('*');
+					stack.pop();
+					i++;
+				}
+			}
+		}
+		else if (name[i] == '*') {
+			if (name[i + 1] == 'a' || name[i + 1] == 'b' || name[i + 1] == 'c') {
+				if (name[i + 2] != '!') {
+					result.push_back(name[i + 1]);
+					result.push_back(name[i]);
+					i++;
+				}
+				else {
+					result.push_back(name[i + 1]);
+					result.push_back(name[i + 2]);
+					result.push_back(name[i]);
+					i += 2;
+				}
+			}
+			else stack.push(name[i]);
+				
+		}
+		else stack.push(name[i]);
+	}
+	while (!stack.empty())
+	{
+		result.push_back(stack.top());
+		stack.pop();
+	}
+	return result;
+}
+
 void transformator()
 {
 	vector<vector<bool>> data { {0,0,0},{0,0,1},{0,1,0},{0,1,1},{1,0,0},{1,0,1},{1,1,0},{1,1,1} };
 	vector<vector<bool>> sdnfprototype;
 	vector<vector<bool>> sknfprototype;
 	vector<bool>number;
-	string func = "a!b!+a!c*!*!";
+	string function = translate();
+	cout << function << endl;
 	int index;
 	for (int i = 0; i < data.size(); i++)
 	{
-		if (encode(func, data[i])) {
+		if (encode(function, data[i])) {
 			sdnfprototype.push_back(data[i]);
 			number.push_back(1);
 		}
@@ -129,7 +224,7 @@ void transformator()
 	}
 	sdnf(sdnfprototype);
 	sknf(sknfprototype);
-	index = translate(number);
+	index = calculator(number);
 	cout << "Index : " << index << endl;
 	cout << "F(" << index << ")(a,b,c) = V";
 	numeral(sdnfprototype);
@@ -137,10 +232,7 @@ void transformator()
 	numeral(sknfprototype);
 }
 
-
-
 int main()
 {
 	transformator();
-	//cout << encode("a!b!+a!c*!*!", { 0,0,0 }) << endl;
 }
