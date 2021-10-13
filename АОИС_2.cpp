346c2d9794b23f83f20685eb6f14cde831c3c338
StoleYourFridge <stoleyourfridge@gmail.com>
2021-10-14 00:26:09 +0300

    File division created

--- АОИС_2.cpp
+++ АОИС_2.cpp
@@ -1,5 +1,93 @@
 ﻿#include "АОИС_2.h"
 
+void deny_to_the_end(string& function)
+{
+	for (int i = 0; i < function.size(); i++)
+	{
+		if (function[i] == ' ') {
+			function.erase(function.begin() + i);
+			i--;
+		}
+	}
+	for (int i = 0; i < function.size(); i++)
+	{
+		if (function[i] == '!' && function[i + 1] == '(') {
+			function.erase(function.begin() + i);
+			stack<char>stack;
+			stack.push('(');
+			int index = i + 1;
+			while (stack.size() != 0)
+			{
+				if (function[index] == '(') stack.push('(');
+				else if (function[index] == ')') stack.pop();
+				index++;
+			}
+			function.insert(function.begin() + index, '!');
+		}
+		else if (function[i] == '!' && (function[i + 1] == 'a' || function[i + 1] == 'b' || function[i + 1] == 'c')) {
+			function.erase(function.begin() + i);
+			function.insert(function.begin() + i + 1, '!');
+		}
+	}
+}
+void translate(string& function)
+{
+	deny_to_the_end(function);
+	stack<char> stack;
+	string result;
+	for (int i = 0; i < function.size(); i++)
+	{
+		if (function[i] == 'a' || function[i] == 'b' || function[i] == 'c') {
+			result.push_back(function[i]);
+		}
+		else if (function[i] == '!') result.push_back('!');
+		else if (function[i] == ')') {
+			while (stack.top() != '(')
+			{
+				result.push_back(stack.top());
+				stack.pop();
+			}
+			stack.pop();
+			if (stack.size() != 0 && stack.top() == '*') {
+				if (function[i + 1] != '!') {
+					result.push_back('*');
+					stack.pop();
+				}
+				else {
+					result.push_back(function[i + 1]);
+					result.push_back('*');
+					stack.pop();
+					i++;
+				}
+			}
+		}
+		else if (function[i] == '*') {
+			if (function[i + 1] == 'a' || function[i + 1] == 'b' || function[i + 1] == 'c') {
+				if (function[i + 2] != '!') {
+					result.push_back(function[i + 1]);
+					result.push_back(function[i]);
+					i++;
+				}
+				else {
+					result.push_back(function[i + 1]);
+					result.push_back(function[i + 2]);
+					result.push_back(function[i]);
+					i += 2;
+				}
+			}
+			else stack.push(function[i]);
+				
+		}
+		else stack.push(function[i]);
+	}
+	while (!stack.empty())
+	{
+		result.push_back(stack.top());
+		stack.pop();
+	}
+	function = result;
+}
+
 int priority(char symbol)
 {
 	if (symbol == '!') return 4;
