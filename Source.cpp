8458f109cbf21c9af9df8fe6454bcc0e8c538874
StoleYourFridge <stoleyourfridge@gmail.com>
2021-10-15 17:06:22 +0300

    merge stash conflict solved

--- Source.cpp
+++ Source.cpp
@@ -2,6 +2,17 @@
 
 void main()
 {
-	tests();
-	system("pause");
+	cout << "1)Tests :: 2)Our own input";
+	int choice;
+	cin >> choice;
+	switch (choice)
+	{
+	case 1: tests();
+		break;
+	case 2: our_own_input();
+		break;
+	default: cout << "Enter something possible to work with!"
+		break;
+	}
+	system("pause"); 
 }
